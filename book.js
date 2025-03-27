811a25c5faf3c286a23bbe3fc1960b297d9f8c82
Makinthan2001 <mahinthan2001a@gmail.com>
2025-03-27 18:17:00 +0530

    uploaded index.html, book.css, book.js

---
+++ book.js
@@ -0,0 +1,43 @@
+var _overlayBox=document.querySelector(".overlayBox")
+var _popupBox=document.querySelector(".popupBox")
+
+function show(){
+    _overlayBox.style.display="block"
+    _popupBox.style.display="block"
+}
+function cancel(){
+    _overlayBox.style.display="none"
+    _popupBox.style.display="none"
+}
+function add(){
+    var _body=document.querySelector(".body")
+    var _ititle=document.getElementById("ititle")
+    var _iauthor=document.getElementById("iauthor")
+    var _idescription=document.getElementById("idescription")
+
+    if(!_ititle.value||!_iauthor.value||!_idescription.value){
+        alert("Fill All Details")
+        return
+    }
+
+    var box=document.createElement("div")
+    box.className="box1"
+
+    box.innerHTML=`<h2>${_ititle.value}</h2>
+                    <small>${_iauthor.value}</small>
+                    <hr>
+                    <p class="ptag">${_idescription.value}</p>
+                    <button onclick="this.parentElement.remove()">Delete</button>`
+    _body.append(box)
+
+    _ititle.value= ""
+    _iauthor.value = ""
+    _idescription.value = ""
+
+    cancel()
+}
+
+function delete_example() {
+    var exampleBox = document.querySelector(".box1");
+    exampleBox.remove()
+}
